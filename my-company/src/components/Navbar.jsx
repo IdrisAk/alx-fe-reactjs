fcc50322f650b026d5cc4e4f319e202c16661181
Idris Ak <idrisakdahiru@gmail.com>
2026-01-18 22:49:34 +0100

    justify content added

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -7,10 +7,12 @@ function Navbar() {
         backgroundColor: '#333',
         padding: '10px',
         display: 'flex',
-        gap: '20px'
+        gap: '20px',
+        justifyContent:'center'
+
       }}
     >
-      <Link style={{ color: '#fff', textDecoration: 'none' }} to="/">Home</Link>
+      <Link style={{ color: '#fff', textDecoration: 'none',  }} to="/">Home</Link>
       <Link style={{ color: '#fff', textDecoration: 'none' }} to="/about">About</Link>
       <Link style={{ color: '#fff', textDecoration: 'none' }} to="/services">Services</Link>
       <Link style={{ color: '#fff', textDecoration: 'none' }} to="/contact">Contact</Link>
